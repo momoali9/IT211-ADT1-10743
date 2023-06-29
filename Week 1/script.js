8c9c39a1c639fce0040b5e546201b4b1df231e32
Mohammed Ali <moali9@yahoo.com.com>
2023-06-28 18:22:44 -0700

    Update Testing

---
+++ Week 1/script.js
@@ -0,0 +1,6 @@
+var firstName = "Mohammed";
+var lastName = "Ali";
+var yearsOfStudy = 1;
+var goal = "Pursue a career in software development";
+
+console.log("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
